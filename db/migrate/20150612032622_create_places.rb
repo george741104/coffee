304f0b795bd0e58789203c8fb7a249aba97ea31f
George Chang <georgec1104@gmail.com>
2015-06-12 11:27:00 +0800

    generated place model

---
+++ db/migrate/20150612032622_create_places.rb
@@ -0,0 +1,12 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.text :description
+      t.string :capacity
+      t.boolean :is_open
+
+      t.timestamps null: false
+    end
+  end
+end
